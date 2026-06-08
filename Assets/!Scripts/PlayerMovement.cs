74ea0c6588185dc62d9e8c564bd45d678ee98932
NitriX3412 <roman.xasan@mail.ru>
2026-06-08 15:08:08 +0300

    Add Rotation

--- Assets/!Scripts/PlayerMovement.cs
+++ Assets/!Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using FishNet.Example.ColliderRollbacks;
 using FishNet.Object;
 using FishNet.Object.Prediction;
 using FishNet.Transporting;
@@ -10,14 +11,17 @@ public class PlayerMovement : NetworkBehaviour
     [SerializeField] private float _speed = 5f;
     [SerializeField] private float _gravity = -9.81f;
     [SerializeField] private InputActionReference _moveRef;
+    [SerializeField] private Camera _playerCamera;
 
     private CharacterController _cc;
     private PlayerNetwork _playerNetwork;
     private float _verticalVelocity;
+    private Quaternion _targetRotation;
 
     public struct MoveData : IReplicateData
     {
         public Vector2 Input;
+        public Quaternion Rotation;
         private uint _tick;
         public void Dispose() { }
         public uint GetTick() => _tick;
@@ -28,6 +32,7 @@ public class PlayerMovement : NetworkBehaviour
     {
         public Vector3 Position;
         public Vector3 Velocity;
+        public Quaternion Rotation;
         private uint _tick;
         public void Dispose() { }
         public uint GetTick() => _tick;
@@ -38,6 +43,14 @@ public class PlayerMovement : NetworkBehaviour
     {
         _cc = GetComponent<CharacterController>();
         _playerNetwork = GetComponent<PlayerNetwork>();
+    }
+
+    private void Update()
+    {
+        if (IsOwner && _playerNetwork.IsAlive.Value)
+        {
+            CalculateTargetRotation();
+        }
     }
 
     public override void OnStartNetwork()
@@ -64,7 +77,8 @@ public class PlayerMovement : NetworkBehaviour
         ReconcileData data = new ReconcileData
         {
             Position = transform.position,
-            Velocity = new Vector3(0f, _verticalVelocity, 0f)
+            Velocity = new Vector3(0f, _verticalVelocity, 0f),
+            Rotation = transform.rotation
         };
         Reconcile(data);
     }
@@ -74,13 +88,15 @@ public class PlayerMovement : NetworkBehaviour
         if (!base.IsOwner) return default;
         return new MoveData
         {
-            Input = _moveRef.action.ReadValue<Vector2>()
+            Input = _moveRef.action.ReadValue<Vector2>(),
+            Rotation = _targetRotation
         };
     }
 
     [Replicate]
     private void RunInputs(MoveData data, ReplicateState state = ReplicateState.Invalid, Channel channel = Channel.Unreliable)
     {
+        transform.rotation = data.Rotation;
         Vector3 move = new Vector3(data.Input.x, 0f, data.Input.y);
         ApplyMovement(ref move);
     }
@@ -99,10 +115,31 @@ public class PlayerMovement : NetworkBehaviour
         _cc.Move(moveDirection * (float)base.TimeManager.TickDelta);
     }
 
+    private void CalculateTargetRotation()
+    {
+        if (_playerCamera == null) return;
+
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = _playerCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (groundPlane.Raycast(ray, out float enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            Vector3 direction = hitPoint - transform.position;
+            direction.y = 0f;
+
+            if (direction.sqrMagnitude > 0.01f)
+            {
+                _targetRotation = Quaternion.LookRotation(direction);
+            }
+        }
+    }
+
     [Reconcile]
     private void Reconcile(ReconcileData data, Channel channel = Channel.Unreliable)
     {
         transform.position = data.Position;
         _verticalVelocity = data.Velocity.y;
+        transform.rotation = data.Rotation;
     }
 }
